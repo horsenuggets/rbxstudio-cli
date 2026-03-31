ea86f52c6dba5b11c05b0ef832c80cf5f99c8e40
HorseNuggets <batchprogrammer314@gmail.com>
2026-03-31 13:40:41 -0500

    Add test-install checks to branch protection

--- Terraform/Main.tf
+++ Terraform/Main.tf
@@ -16,4 +16,16 @@ provider "github" {
 module "repo" {
   source     = "../Submodules/luau-cicd/Terraform/Modules/CommandlineCli"
   repository = "rbxstudio-cli"
+
+  extra_main_checks = [
+    "Test install on linux",
+    "Test install on macos",
+    "Test install on windows",
+  ]
+
+  extra_release_checks = [
+    "Test install on linux",
+    "Test install on macos",
+    "Test install on windows",
+  ]
 }
